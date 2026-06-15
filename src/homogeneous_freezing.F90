73445d0c3a111030f0f02120da55c28b72848718
paulfield2024 <paul.field@metoffice.gov.uk>
2026-06-15 14:27:53 +0100

    change to ni limit and lower it

--- src/homogeneous_freezing.F90
+++ src/homogeneous_freezing.F90
@@ -207,8 +207,8 @@ contains
 
     real(wp) :: Tk, cap, rhoi, Ei, Ew, bm, Ai, B0, Bis, aw, dnumberi, &
                 ka, min_homog_ni, dniraw, d0_homog
-    logical :: l_use_critical_w = .True.  !ni controlled by w and environmental conditions
-    logical :: l_use_ni_limit = .False.   !ni limited to max per timestep
+    logical :: l_use_critical_w = .False.  !ni controlled by w and environmental conditions
+    logical :: l_use_ni_limit = .True.   !ni limited to max per timestep
     
     
     logical :: l_condition
@@ -242,7 +242,7 @@ contains
                 
                 !limit production to 1/cc in timestep - do this properly with KL but need to adapt for droplets
                 if (l_use_ni_limit) then
-                   dnumberi=min(dnumber, 1e2)
+                   dnumberi=min(dnumber, 1e1)
                    dmass=qfields(k, i_ql)*dnumberi/qfields(k, i_nl)
                 endif
                 
